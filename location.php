8c233605ffde02132208866a467f7272fb7b8cc1
Herman Sophian Putra <herman_sophian@yahoo.com>
2013-05-11 18:15:25 +0700

    Penyesuaian konten

--- location.php
+++ location.php
@@ -54,36 +54,64 @@
                 <div class="row">
                     <div class="span12">
                         <i class="icon-envelope-alt page-title-icon"></i>
-                        <h2>Contact Us /</h2>
-                        <p>Here is how you can contact us</p>
+                        <h2>Location /</h2>
+                        <p>Follow the easy way to find us.</p>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- Contact Us -->
+        <!-- Location -->
         <div class="contact-us container">
             <div class="row">
                 <div class="contact-form span7">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper <a href="">suscipit lobortis</a> nisl ut aliquip ex ea commodo consequat.</p>
-                    <form method="post" action="assets/sendmail.php">
-                        <label for="name" class="nameLabel">Name</label>
-                        <input id="name" type="text" name="name" placeholder="Enter your name...">
-                        <label for="email" class="emailLabel">Email</label>
-                        <input id="email" type="text" name="email" placeholder="Enter your email...">
-                        <label for="subject">Subject</label>
-                        <input id="subject" type="text" name="subject" placeholder="Your subject...">
-                        <label for="message" class="messageLabel">Message</label>
-                        <textarea id="message" name="message" placeholder="Your message..."></textarea>
-                        <button type="submit">Send</button>
-                    </form>
+                    <ul class="portfolio-img">
+                        <center>
+                    <li data-id="p-1" data-type="logo-design" class="span3">
+                        <div class="work">
+                            <a href="assets/img/location/airport.jpg" rel="prettyPhoto">
+                                <img src="assets/img/location/airport.jpg" alt="">
+                            </a>
+                            <h4>Adi Soemarmo International Airport</h4>
+                            <p>Located 9,5 KM drive or 16 Minutes from Adi Soemarmo Airport.</p>
+                        </div>
+                    </li>
+                    <li data-id="p-2" data-type="logo-design" class="span3">
+                        <div class="work">
+                            <a href="assets/img/location/train.jpg" rel="prettyPhoto">
+                                <img src="assets/img/location/train.jpg" alt="">
+                            </a>
+                            <h4>Stasiun Kereta Api <br>Solo Balapan</h4>
+                            <p>Get home in 10 Minutes drive or 4,3 KM from Solo Balapan Train Station.</p>
+                        </div>
+                    </li>
+                    <li data-id="p-3" data-type="web-design" class="span3">
+                        <div class="work">
+                            <a href="assets/img/location/solosqr.jpg" rel="prettyPhoto">
+                                <img src="assets/img/location/solosqr.jpg" alt="">
+                            </a>
+                            <h4>Solo Square Mall <br> The lifestyle center</h4>
+                            <p>In only 8 Minutes or 3,3 KM, you can arrive at Solo Square Mall.</p>
+                        </div>
+                    </li>
+                    <li data-id="p-4" data-type="logo-design" class="span3">
+                        <div class="work">
+                            <a href="assets/img/location/solopar.jpg" rel="prettyPhoto">
+                                <img src="assets/img/location/solopar.jpg" alt="">
+                            </a>
+                            <h4>Solo Paragon Mall <br> The Pride of Java</h4>
+                            <p>You will only need 8 Minutes or 3,2 KM to get to Solo Paragon Mall.</p>
+                        </div>
+                    </li>
+                    </center>
+                    </ul>
                 </div>
                 <div class="contact-address span5">
-                    <h4>We Are Here</h4>
+                    <h5 style="text-align: center   ">Distance and time are based on Google Maps Direction</h5>
                     <div class="map"></div>
                     <h4>Address</h4>
-                    <p>Via Principe Amedeo 9 <br> 10100, Torino, TO, Italy</p>
-                    <p>Phone: 0039 333 12 68 347</p>
+                    <p>Jalan Pakel, Sumber <br> 57138, Surakarta, Jawa Tengah</p>
+                    <p>Phone: 0271 - 72 47 47</p>
                 </div>
             </div>
         </div>
